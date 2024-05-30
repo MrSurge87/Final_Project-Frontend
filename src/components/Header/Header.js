ca828fbb60e1c768282ac8ed8f287866ff145210
MrSurge87 <Mendozsa.sm@gmail.com>
2024-05-30 17:51:31 -0400

    changed background image and adjusted home buttons

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,19 +1,15 @@
 import "./Header.css";
 
 const Header = () => {
-
-return (
-    
+  return (
     <header className="Header">
-    <p className="Header__title">News Explorer</p>
-    <div className="Header__buttons">
-    <button className="Home__button">Home</button>
-    <button className="SignIn__button">Sign In</button>
-    </div>
-    
-  </header>
-  
-)
-}
+      <p className="Header__title">News Explorer</p>
+      <div className="Header__buttons">
+        <button className="Home__button">Home</button>
+        <button className="SignIn__button">Sign In</button>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
