e190a977371f84d0a182fe4424fce89fb215ad85
MrSurge87 <Mendozsa.sm@gmail.com>
2024-05-29 23:04:03 -0400

    created header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,19 @@
+import "./Header.css";
+
+const Header = () => {
+
+return (
+    
+    <header className="Header">
+    <p className="Header__title">News Explorer</p>
+    <div className="Header__buttons">
+    <button className="Home__button">Home</button>
+    <button className="SignIn__button">Sign In</button>
+    </div>
+    
+  </header>
+  
+)
+}
+
+export default Header;
