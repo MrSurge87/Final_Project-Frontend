b1c5524a0d1b2ad7ba35e77b20548344f631d58f
MrSurge87 <Mendozsa.sm@gmail.com>
2024-06-02 14:59:56 -0400

    added about author image

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,5 +1,17 @@
 import "./Main.css";
 
-const Main = () => {};
+const Main = () => {
+  return (
+    <div className="Main">
+      <div className="About">
+        <img
+          className="About-author__image"
+          src="../src/images/about-author-image.svg"
+          alt="About Author"
+        />
+      </div>
+    </div>
+  );
+};
 
 export default Main;
