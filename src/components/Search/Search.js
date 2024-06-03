28ecdfb98765303cdfaf0d753a241e2f5e692e8c
MrSurge87 <Mendozsa.sm@gmail.com>
2024-06-03 18:31:57 -0400

    added search box and text for search prompt

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -3,13 +3,14 @@ import "./Search.css";
 const Search = () => {
   return (
     <div className="Searches">
-      <h1 className="Search__title">What's going on in the world?</h1>
-      <p className="Search__description">
-        Find the latest news on any topic and save them in your personal account
-      </p>
-      <input className="Search__bar-input" placeholder="Enter Topic"></input>
-      
-      
+      <div className="Search__box">
+        <h1 className="Search__title">What's going on in the world?</h1>
+        <p className="Search__description">
+          Find the latest news on any topic and save them in your personal
+          account
+        </p>
+        <input className="Search__bar-input" placeholder="Enter Topic"></input>
+      </div>
     </div>
   );
 };
