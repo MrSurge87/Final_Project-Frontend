0d57a988d61d5313fdfc5c2d173a0a72ff318836
MrSurge87 <Mendozsa.sm@gmail.com>
2024-06-02 21:57:58 -0400

    added about author description

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -8,7 +8,15 @@ const Main = () => {
         <div className="About-author">
           <h1 className="About-author__title">About The Author</h1>
           <p className="About-author__description">
-            This project was created by Sergio A Mendoza.
+            Hello! My name is Sergio A Mendoza. Welcome to my TripleTen final project.<br></br>
+            I am a full stack web developer. I have learned how to create online apps such as <br></br>
+            this one with tools like JavaScript, React, Trello for work flow management, Figma,<br></br>
+            and others.<br></br>
+            <br></br>
+            I have learned full stack web development from TripleTen. I loved learning new skills<br></br>
+            and new ways to apply old ones. It was exciting to learn how to jump into the world <br></br>
+            of software engineering so that I can be a part of building future applications for <br></br>
+            the business I want to be a part of.
           </p>
         </div>
       </div>
