8b136d0a403709af58d1c9c4489db75c9ea9bad4
MrSurge87 <Mendozsa.sm@gmail.com>
2024-06-03 17:03:38 -0400

    added search bar input and text

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 import Header from "./components/Header/Header";
 import Main from "./components/Main/Main";
 import Footer from "./components/Footer/Footer";
+import Search from "./components/Search/Search";
 
 function App() {
   return (
     <div className="App">
       <div className="Search">
       <Header />
+      <Search />
       </div>
       <Main />
       <Footer />
