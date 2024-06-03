e38744b45461b7257bc446d96be69260088056ba
MrSurge87 <Mendozsa.sm@gmail.com>
2024-06-03 16:04:26 -0400

    made changes to header buttons to match figma styling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,10 +4,15 @@ const Header = () => {
   return (
     <header className="Header">
       <p className="Header__title">News Explorer</p>
+      
       <div className="Header__buttons">
         <button className="Home__button">Home</button>
         <button className="SignIn__button">Sign In</button>
+        <div className="Home__button-border-bottom"></div>
       </div>
+      
+      
+      
     </header>
   );
 };
