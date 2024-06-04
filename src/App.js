a13b3dee13396b13bea7339170b76888620c7b6e
MrSurge87 <Mendozsa.sm@gmail.com>
2024-06-04 18:19:13 -0400

    created SignIn form and added popup

--- src/App.js
+++ src/App.js
@@ -4,13 +4,45 @@ import Header from "./components/Header/Header";
 import Main from "./components/Main/Main";
 import Footer from "./components/Footer/Footer";
 import Search from "./components/Search/Search";
+import SignInModal from "./components/SignInModal/SignInModal";
+
+//React Imports
+import { useEffect, useState } from "react";
 
 function App() {
+  const [activeModal, setActiveModal] = useState("");
+
+  const handleCreateModal = () => {
+    setActiveModal("create");
+  }
+  const handleOpenSignInModal = () => {
+    setActiveModal("SignIn");
+  };
+
+  const handleOpenRegisterModal = () => {
+    setActiveModal("register");
+  };
+
+  const handleCloseModal = () => {
+    setActiveModal("");
+  };
+
   return (
     <div className="App">
       <div className="Search">
-      <Header />
-      <Search />
+        <Header
+          onSignIn={handleOpenSignInModal}
+          onRegister={handleOpenRegisterModal}
+          onCreateModal={handleCreateModal}
+        />
+        {activeModal === "SignIn" && (
+          <SignInModal
+            onClose={handleCloseModal}
+            openSignInModal={handleOpenSignInModal}
+            openRegisterModal={handleOpenRegisterModal}
+          />
+        )}
+        <Search />
       </div>
       <Main />
       <Footer />
--- src/components/SignInModal/SignInModal.js
+++ src/components/SignInModal/SignInModal.js
@@ -1,12 +1,27 @@
 import React, { useState } from "react";
 import ModalWithForm from "../ModalWithForm/ModalWithForm";
 
-const SignInModal = ({ onClose, openRegisterModal, isLoading }) => {
+const SignInModal = ({ onClose, openRegisterModal, isLoading, signInUser }) => {
+
+    const [email, changeEmail ] = useState("");
+    const handleEmailChange = (e) => {
+        changeEmail(e.target.value);
+    }
+
+    const [password, changePassword] = useState("");
+    const handlePasswordChange = (e) => {
+        changePassword(e.target.value)
+    }
+
+    const onSignIn = (e) => {
+        e.preventDefault();
+        signInUser({ email, password })
+    }
   return (
     <ModalWithForm
       title="Sign In"
       onClose={onClose}
-      onSubmit={OnSignIn}
+      onSubmit={onSignIn}
       buttonText="Sign In"
       name="SignIn"
     >
@@ -24,6 +39,7 @@ const SignInModal = ({ onClose, openRegisterModal, isLoading }) => {
             placeholder="Email"
             id="email"
             value={email}
+            onChange={handleEmailChange}
             required
           />
         </li>
@@ -39,6 +55,9 @@ const SignInModal = ({ onClose, openRegisterModal, isLoading }) => {
             minLength="1"
             maxLength="50"
             value={password}
+            onChange={handlePasswordChange}
+            requried
+            
             required />
         </li>
       </ul>
