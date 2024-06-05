b89a0d00d1e8033a3c1fc499d31428e586ecfc3e
MrSurge87 <Mendozsa.sm@gmail.com>
2024-06-04 20:37:18 -0400

    added css to signin modal

--- src/components/ModalWithForm/ModalWithForm.js
+++ src/components/ModalWithForm/ModalWithForm.js
@@ -4,10 +4,8 @@ const ModalWithForm = ({ children, title, onClose, name, onSubmit }) => {
   return (
     <div ckassName={`modal modal_type_${name}`}>
       <div className="modal__content">
-        <h3 className="add__garment-header">
-          {title}
-          <button classname="close" type="button" onClick={onClose}></button>
-        </h3>
+        <button className="close__button" type="button" onClick={onClose}></button>
+        <h3 className="modal-form-title">{title}</h3>
         <form onSubmit={onSubmit} className="modal-form">
           {children}
         </form>
