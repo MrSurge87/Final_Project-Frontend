17eaade424f01a31a8d76191025cc58e33ca9203
MrSurge87 <Mendozsa.sm@gmail.com>
2024-06-13 21:37:55 -0400

    created Sign Up Modal

---
+++ src/components/SignUpModal/SignUpModal.js
@@ -0,0 +1,114 @@
+import ModalWithForm from "../ModalWithForm/ModalWithForm";
+import { useState } from "react";
+
+const SignUpModal = (onClose, openSignInModal, isLoading, registerUser) => {
+  const [email, setEmail] = useState("");
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const [password, setPassword] = useState("");
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const [name, setName] = useState("");
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const onRegister = (e) => {
+    e.preventDefault();
+    registerUser({ email, password, name });
+  };
+
+  return (
+    <ModalWithForm
+      name="register"
+      title="Sign Up"
+      onClose={onClose}
+      buttonText="Sign Up"
+      onSubmit={onRegister}
+    >
+      <div>
+        <label className="modal__input-title" htmlFor="email">
+          Email*
+        </label>
+        <input
+          id="email"
+          className="input"
+          type="email"
+          name="email"
+          placeholder="Email"
+          minLength="1"
+          maxLength="50"
+          required
+          value={email}
+          onChange={handleEmailChange}
+        ></input>
+      </div>
+
+      <div>
+        <label className="modal__input-title" htmlFor="password">
+          Password
+        </label>
+        <input
+          id="password"
+          className="input"
+          type="password"
+          name="password"
+          placeholder="Password"
+          minLength="1"
+          maxLength="50"
+          required
+          value={password}
+          onChange={handlePasswordChange}
+        ></input>
+      </div>
+
+      <div>
+        <label className="modal__input-title" htmlFor="name">
+          Name
+        </label>
+        <input
+          id="name"
+          className="input"
+          tpye="text"
+          name="name"
+          placeholder="Name"
+          minLength="1"
+          maxLength="50"
+          required
+          value={name}
+          onChange={handleNameChange}
+        ></input>
+      </div>
+
+      <div>
+        <label className="modal__input-title" htmlFor="avatar">
+          Avatar URL
+        </label>
+        <input
+          id="avatar"
+          className="input"
+          type="url"
+          name="url"
+          placeholder="Avatar URL"
+          required
+          value={avatar}
+          onChange={handleAvatarChange}
+        ></input>
+      </div>
+      <div className="modal-form-buttons">
+        <button className="modal-form-submit" type="submit">
+          {isLoading ? "Submitting..." : "Sign Up"}
+        </button>
+        <button className="modal__login" type="button" onClick={openLoginModal}>
+          Or Log In
+        </button>
+      </div>
+    </ModalWithForm>
+  );
+};
+
+export default SignUpModal;
