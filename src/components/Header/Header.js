19472cf54769650d5e7322d5d359c1cfcdc2be67
MrSurge87 <Mendozsa.sm@gmail.com>
2024-06-19 15:08:36 -0400

    reverted to older code that worked

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,34 @@
 import "./Header.css";
+import NewsExplorerWhite from "../../images/NewsExplorer-White.svg";
+import { useContext } from "react";
+import { NavLink } from "react-router-dom";
+import { CurrentUserContext } from "../../contexts/CurrentUserContext";
 
-const Header = ({ onSignIn }) => {
+const currentDate = new Date().toLocaleString("default", {
+  month: "long",
+  day: "numeric",
+});
+
+const Header = ({ onSignIn, signedIn }) => {
+  const currentUser = useContext(CurrentUserContext);
+
   return (
     <header className="Header">
-      <p className="Header__title">News Explorer</p>
-      
-      <div className="Header__buttons">
-        <button className="Home__button" type="button" >Home</button>
-        <button className="SignIn__button" type="button" onClick={onSignIn}>Sign In</button>
-        <div className="Home__button-border-bottom"></div>
+      <div>
+        <NavLink to="/" className="Header__title">
+          <img src={ NewsExplorerWhite } alt="logo" />
+        </NavLink>
+        
       </div>
+      
+      
+ <div className="Header__buttons">
+ <button className="Home__button" type="button" >Home</button>
+ <button className="SignIn__button" type="button" onClick={onSignIn}>Sign In</button>
+ <div className="Home__button-border-bottom"></div>
+</div>
+      
+     
       
       
       
