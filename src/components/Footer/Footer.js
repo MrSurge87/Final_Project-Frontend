deef5919315d4620de5ba5e79428689b7270924c
MrSurge87 <Mendozsa.sm@gmail.com>
2024-06-26 11:24:20 -0400

    changed footer to include links

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,22 +1,30 @@
 import "./Footer.css";
+import GitHubIcon from "../../images/github-image.svg";
+import FacebookIcon from "../../images/Facebook-image.svg";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
+
+    const currentYear = new Date().getFullYear();
     return (
-        <div className="Footer">
-            <p className="Copyright">© Sergio A Mendoza, Powered by News Api</p>
-            <div className="Footer-buttons">
-                <button className="Footer-home__button" type="button">Home</button>
-                <a href="https://tripleten.com/" target="_blank">
-                <button className="Footer-TripleTen__button" type="button">TripleTen</button>
+        <footer className="Footer">
+            <p className="footer__text">{currentYear} © Sergio A Mendoza, Powered by News Api</p>
+
+            <div className="footer__links">
+                <Link to="/">
+                <button className="footer__button-home" type="button">Home</button>
+                </Link>
+                <a href="https://tripleten.com/" target="_blank" className="footer__button-tripleten" >
+                TripleTen
                 </a>
-                <a href="https://github.com/MrSurge87" target="_blank">
-                <button className="Github__button" type="button"></button>
+                <a href="https://github.com/MrSurge87" target="_blank" className="footer__button-github">
+                <img src={GitHubIcon}  alt="GitHub"/>
                 </a>
-                <a href="https://www.facebook.com/sergio.a.mendoza.54/" target="_blank">
-                <button className="Facebook__button" type="button"></button>
+                <a href="https://www.facebook.com/sergio.a.mendoza.54/" target="_blank" className="footer__button-facebook">
+                 <img src={FacebookIcon} alt="Facebook"/>
                 </a>
             </div>
-        </div>
+        </footer>
     )
 }
 
