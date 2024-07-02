b411bfb761a0866679560cf1310407a5cb58caec
MrSurge87 <Mendozsa.sm@gmail.com>
2024-07-02 15:02:28 -0400

    updated news api

--- src/utils/NewsApi.js
+++ src/utils/NewsApi.js
@@ -1,8 +1,13 @@
 import { APIkey, parseCurrentDate, parsePreviousWeek } from "./constants";
 import { processServerResponse } from "./utils";
 
+
 export const getSearchResults = (keyword) => {
-  return fetch(
+  const newsApi =
+   fetch(
     `https://newsapi.org/v2/everthing?q=${keyword}&from=${parsePreviousWeek}&to=${parseCurrentDate}&pageSize=100&sortBy=popularity&apiKey=${APIkey}`
-  ).then(processServerResponse);
+  ).then((res) => {
+    return processServerResponse(res);
+  });
+  return newsApi;
 };
