53d32941480ecd2fc8e727760d0cd36603704077
MrSurge87 <Mendozsa.sm@gmail.com>
2024-07-17 22:28:20 -0400

    updated footer for mobile device resolution

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,28 +4,47 @@ import FacebookIcon from "../../images/Facebook-image.svg";
 import { NavLink } from "react-router-dom";
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
+  return (
+    <footer className="footer">
+      <p className="footer__text">
+        {currentYear} © Sergio A Mendoza, Powered by News Api
+      </p>
 
-    const currentYear = new Date().getFullYear();
-    return (
-        <footer className="footer">
-            <p className="footer__text">{currentYear} © Sergio A Mendoza, Powered by News Api</p>
-
-            <div className="footer__links">
-                <NavLink to="/">
-                <button className="footer__button-home" type="button">Home</button>
-                </NavLink>
-                <a href="https://tripleten.com/" target="_blank" className="footer__button-tripleten" >
-                TripleTen
-                </a>
-                <a href="https://github.com/MrSurge87" target="_blank" className="footer__button-github">
-                <img src={GitHubIcon}  alt="GitHub"/>
-                </a>
-                <a href="https://www.facebook.com/sergio.a.mendoza.54/" target="_blank" className="footer__button-facebook">
-                 <img src={FacebookIcon} alt="Facebook"/>
-                </a>
-            </div>
-        </footer>
-    )
-}
+      <div className="footer__links">
+        <div className="footer__links-A">
+          <NavLink to="/">
+            <button className="footer__button-home" type="button">
+              Home
+            </button>
+          </NavLink>
+          <a
+            href="https://tripleten.com/"
+            target="_blank"
+            className="footer__button-tripleten"
+          >
+            TripleTen
+          </a>
+        </div>
+        <div className="footer__links-B">
+          <a
+            href="https://github.com/MrSurge87"
+            target="_blank"
+            className="footer__button-github"
+          >
+            <img src={GitHubIcon} alt="GitHub" />
+          </a>
+          <a
+            href="https://www.facebook.com/sergio.a.mendoza.54/"
+            target="_blank"
+            className="footer__button-facebook"
+          >
+            <img src={FacebookIcon} alt="Facebook" />
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
