0af96077188d6d9f663349516bed335a331b864c
MrSurge87 <Mendozsa.sm@gmail.com>
2024-08-12 20:37:36 -0400

    fix: footer html

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -54,38 +54,6 @@ const Footer = () => {
         </ul>
       </nav>
 
-      {/* <div className="footer__links">
-        <div className="footer__links-A">
-          <NavLink to="/">
-            <button className="footer__button-home" type="button">
-              Home
-            </button>
-          </NavLink>
-          <a
-            href="https://tripleten.com/"
-            target="_blank"
-            className="footer__button-tripleten"
-          >
-            TripleTen
-          </a>
-        </div>
-        <div className="footer__links-B">
-          <a
-            href="https://github.com/MrSurge87"
-            target="_blank"
-            className="footer__button-github"
-          >
-            <img src={GitHubIcon} alt="GitHub" />
-          </a>
-          <a
-            href="https://www.facebook.com/sergio.a.mendoza.54/"
-            target="_blank"
-            className="footer__button-facebook"
-          >
-            <img src={FacebookIcon} alt="Facebook" />
-          </a>
-        </div>
-      </div> */}
     </footer>
   );
 };
