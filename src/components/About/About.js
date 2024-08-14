a6de5ed15ca9a7024fe5dddb97993ed1172de4df
MrSurge87 <Mendozsa.sm@gmail.com>
2024-08-13 23:53:33 -0400

    fix: updated BEM class name

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -7,14 +7,14 @@ const About = () => {
       <div className="about__container">
         <img src={AboutAuthorImage} className="about__image" alt="Author" />
         <div className="about__text">
-          <h2 className="about__text-title">About The Author</h2>
-          <p className="about__text-description">
+          <h2 className="about__text_title">About The Author</h2>
+          <p className="about__text_description">
             Hello! My name is Sergio A Mendoza. Welcome to my TripleTen final
             project. I am a full stack web developer. I have learned how to
             create online apps such as this one with tools like JavaScript,
             React, Trello for work flow management, Figma, and others.
           </p>
-          <p className="about__text-description">
+          <p className="about__text_description">
             I have learned full stack web development from TripleTen. I loved
             learning new skills and new ways to apply old ones. It was exciting
             to learn how to jump into the world of software engineering so that
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -11,10 +11,10 @@ const Footer = () => {
         {currentYear} © Sergio A Mendoza, Powered by News Api
       </p>
       <nav className="footer__links">
-        <ul className="footer__links-description">
+        <ul className="footer__links_description">
           <li className="footer__link">
             <NavLink to="/">
-              <button className="footer__button footer__button-home" type="button">
+              <button className="footer__button footer__button_home" type="button">
               Home
               </button>
             </NavLink>
@@ -23,29 +23,29 @@ const Footer = () => {
             <a
               href="https://tripleten.com/"
               target="_blank"
-              className="footer__button footer__button--tripleten"
+              className="footer__button footer__button_tripleten"
               rel="noopener noreferrer"
             >
               TripleTen
             </a>
           </li>
           </ul>
-          <ul className="footer__links-icons">
-          <li className="footer__icon">
+          <ul className="footer__links_type_icons">
+          <li className="footer__links_icon">
             <a
               href="https://github.com/MrSurge87"
               target="_blank"
-              className="footer__Button footer__button--github"
+              className="footer__Button footer__button_github"
               rel="noopener noreferrer"
             >
               <img src={GitHubIcon} alt="GitHub" />
             </a>
             </li>
-            <li className="footer__icon">
+            <li className="footer__links_icon">
             <a
               href="https://www.facebook.com/sergio.a.mendoza.54/"
               target="_blank"
-              className="footer__button footer__button--facebook"
+              className="footer__button footer__button_facebook"
               rel="noopener noreferrer"
             >
               <img src={FacebookIcon} alt="Facebook" />
